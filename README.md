12b33a06035f7811e16b22c69c81fdbbd49006ad
diwes-gh <desarrollo@diwes.es>
2023-07-28 18:37:31 +0000

    Publicación del código
    Signed-off-by: diwes-gh <desarrollo@diwes.es>

--- README.md
+++ README.md
@@ -1,1 +1,22 @@
+<div align="center">
+  <img src="https://apps.diwes.es/img/icon-chatgpt-diwes.png" alt="LOGO ALEJANDROALSA" height="200">
+  <img src="https://apps.diwes.es/assets/img/Logos%20DIWES/DIWES%20Logo%20-%20Blanco.svg" alt="LOGO OS" height="160" style="margin-left: 100px;">
+</div>
+
 # ChatGPT DIWES
+
+En este proyecto hemos utilizado la API de chatgpt para codificar un bot de chatgpt con javascript.
+
+## Link
+
+[ChatGPT DIWES](https://apps.diwes.es/chat-gpt-diwes/index.html)
+
+## Descarga 
+
+```bash
+git clone https://github.com/diwes-gh/chatgpt.git
+```
+
+## Desarrolladores
+
+[@dpto.desarrollo.diwes](https://www.diwes.es)
--- index.js
+++ index.js
@@ -3,14 +3,12 @@ const chatLog = document.getElementById('chat-log'),
     sendButton = document.getElementById('send-button'),
     buttonIcon = document.getElementById('button-icon'),
     info = document.querySelector('.info');
-
 sendButton.addEventListener('click', sendMessage);
 userInput.addEventListener('keydown', (event) => {
     if (event.key === 'Enter') {
         sendMessage();
     }
 });
-
 function sendMessage() {
     const message = userInput.value.trim();
     if (message === '') {
@@ -26,10 +24,8 @@ function sendMessage() {
         }, 2000);
         return;
     }
-
     appendMessage('user', message);
     userInput.value = '';
-
     const options = {
         method: 'POST',
         headers: {
@@ -53,22 +49,18 @@ function sendMessage() {
         }
     });
 }
-
 function appendMessage(sender, message) {
     info.style.display = "none";
     buttonIcon.classList.remove('fa-solid', 'fa-paper-plane');
     buttonIcon.classList.add('fas', 'fa-spinner', 'fa-pulse');
-
     const messageElement = document.createElement('div');
     const iconElement = document.createElement('div');
     const chatElement = document.createElement('div');
     const icon = document.createElement('i');
-
     chatElement.classList.add("chat-box");
     iconElement.classList.add("icon");
     messageElement.classList.add(sender);
     messageElement.innerText = message;
-
     if (sender === 'user') {
         icon.classList.add('fa-regular', 'fa-user');
         iconElement.setAttribute('id', 'user-icon');
@@ -76,11 +68,9 @@ function appendMessage(sender, message) {
         icon.classList.add('fa-solid', 'fa-robot');
         iconElement.setAttribute('id', 'bot-icon');
     }
-
     iconElement.appendChild(icon);
     chatElement.appendChild(iconElement);
     chatElement.appendChild(messageElement);
     chatLog.appendChild(chatElement);
     chatLog.scrollTo = chatLog.scrollHeight;
-
 }
